fd7ff61c58a69526c63f90422c68aaddfc795ab9
Ilgova <ilgova.1594@mail.ru>
2023-01-25 23:49:00 +0300

    HomeWork 002

---
+++ 013/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введи трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number > 100)
+    {
+        Number = Number % 10;
+    Console.WriteLine("третья цифра числа " + Number);
+    }
+else
+    Console.WriteLine("третьей цифры нет");
---
+++ 015/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру, обозначающую день недели от 1 до 7");
+int NumberDay = Convert.ToInt32(Console.ReadLine());
+
+if  (NumberDay == 6 || NumberDay == 7)
+ {
+    Console.WriteLine("Да");
+ } 
+    
+else if (NumberDay < 1 || NumberDay > 7)
+    {
+        Console.WriteLine("Недопустимое значение. Попробуйте ещё раз");
+ } 
+else 
+Console.WriteLine("нет");
